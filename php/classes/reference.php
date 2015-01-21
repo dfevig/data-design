1ba9ae41b53dbdba031a840cc8a6a54e9e4e84fd
David Fevig <dafevig@gmail.com>
2015-01-21 16:51:53 -0700

    added class

--- php/classes/reference.php
+++ php/classes/reference.php
@@ -19,7 +19,7 @@ class Reference {
 	/**
 	 * journal/article name, this is a foreign key
 	 */
-	private $journal;
+	private $journalName;
 		/**
 	 * link type used for reference, this is a foreign key
 	 */
@@ -27,12 +27,33 @@ class Reference {
 	/**
 	 * numerical page number
 	 */
-	private $page;
+	private $pageNum;
 
 	/**
 	 * constructor for Reference
-	 * PLACE HOLDER FOR CONSTRUCTOR
-	 */
+	 *
+	 * @param int $newReferenceId id of the reference
+	 * @param string $newAuthor string containing the author name
+	 * @param string $newJournalName string containing the journal name
+	 * @param int $newPageNum int of the page number
+	 * @param string $newLinkType string of the link type associated
+	 * @throws InvalidArgumentException if data types are not valid
+	 * @thorws RangeException if data values are out of bounds (e.g., incorrect strings, negative numbers)
+	 **/
+	public function __construct($newReferenceId, $newAuthor, $newJournalName, $newPageNum, $newLinkType) {
+		try {
+			$this->setReferenceId($newReferenceId);
+			$this->setAuthor($newAuthor);
+			$this->setJournalName($newJournalName);
+			$this->setPageNum($newPageNum);
+			$this->setLinkType($newLinkType);
+		} catch(InvalidArgumentException $invalidArgument){
+		throw(new InvalidArgumentException($invalidArgument->getMessage(),0,$invalidArgument));
+	}	catch(RangeException $range) {
+		throw(new RangeException($range->getMessage(), 0 , $range));
+	}
+	}
+
 
 	/**
 	 * accessor method to reference id
@@ -48,7 +69,7 @@ class Reference {
 	 * @param int $newReferenceId new value of reference id
 	 * @throws InvalidArgumentException if $newReferenceId is not and integer
 	 * @throws RangeException if $newReferenceId is not positive
-	 */
+	 **/
 	public function setReferenceId($newReferenceId) {
 		// base case: if the reference id is null, this reference without a mySQL assigned id (yet)
 		if($newReferenceId === null) {
@@ -69,7 +90,115 @@ class Reference {
 		// convert and store the reference id
 		$this->referenceId = intval($newReferenceId);
 	}
+	/**
+	 * accessor method for author
+	 *
+	 * @return string value for author
+	 **/
+	public function getAuthor() {
+		return($this->author);
+	}
 
-}
+	/**
+	 * mutator method for author
+	 *
+	 * @param string $newAuthor new value for author
+	 * @throws InvalidArgumentException if $newAuthor is not a string or insecure
+	 **/
+	public function setAuthor($newAuthor) {
+		// verify the author entered is secure
+		$newAuthor = trim($newAuthor);
+		$newAuthor = filter_var($newAuthor, FILTER_SANITIZE_STRING);
+		if(empty($newAuthor) === true) {
+			throw(new InvalidArgumentException("Author name is empty or insecure"));
+		}
+		//store the tweet content
+		$this->author = $newAuthor;
+	}
+	/**
+	 * accessor method for journal name
+	 *
+	 * @return string value for journal name
+	 **/
+	public function getJournalName() {
+		return($this->journalName);
+	}
+	/**
+	 * mutator method for journal name
+	 *
+	 * @param string $newJournalName new value for the journal name
+	 * @throws InvalidArgumentException if $newJournalName is not a string or insecure
+	 **/
+	public function setJournalName($newJournalName) {
+		$newJournalName = trim($newJournalName);
+		$newJournalName = filter_var($newJournalName, FILTER_SANITIZE_STRING);
+		if(empty($newJournalName) === true) {
+			throw(new InvalidArgumentException("Journal name is empty or insecure"));
+		}
+		//store the journal name
+		$this->journalName = $newJournalName;
+	}
+	/**
+	 * accessor method for the page number
+	 *
+	 * @return int value for page number
+	 **/
+	public function getPageNum(){
+		return($this->pageNum);
+	}
+	/**
+	 * mutator method for the page number
+	 *
+	 * @param int $newPageNum new value of page number
+	 * @throws InvalidArgumentException if $newPageNum is not and integer
+	 * @throws RangeException if $newPageNum is not positive
+	 **/
+	public function setPageNum($newPageNum) {
+		//verify the page number is valid
+		$newPageNum = filter_var($newPageNum, FILTER_VALIDATE_INT);
+		if($newPageNum === false) {
+				throw(new InvalidArgumentException("Page number is not a valid integer"));
+		}
+		//verify page number is positive
+		if($newPageNum <= 0) {
+			throw(new RangeException("the page number is not a positive"));
+		}
+		//convert and store the page number
+		$this->pageNumv = intval($newPageNum);
+	}
+	/**
+	 * accessor method for the link type
+	 *
+	 * @return string value of reference id
+	 */
+	public function linkType(){
+		return($this->linkType);
+	}
+	/**
+	 * mutator method for link type
+	 *
+	 * @param string $newLinkType new value of link type
+	 * @throws InvalidArgumentException if $newLinkType is not a string
+	 * @throws InvalidArgumentException if$newLinkType is not URL
+	 **/
+	public function setLinkType($newLinkType) {
+		//verify the link type is valid
+		$newLinkType = trim($newLinkType);
+		$newLinkType = filter_var(($newLinkType, FILTER_VALIDATE_URL));
+		if(empty($newLinkType) === true){
+			throw(new InvalidArgumentException("link type content empty or invalid"));
+		}
+	public function setLinkType($newLinkType) {
+			//verify the link type URL is valid
+			$newLinkType = trim($newLinkType);
+			$newLinkType = filter_var(($newLinkType, FILTER_SANITIZE_URL));
+		if($newLinkType === false){
+			throw(new InvalidArgumentException("link type URL is invalid"));
+		}
+		//convert and store link type
+		$this->linkType = intval($newLinkType);
+	}
+
+	}
 
 ?>
